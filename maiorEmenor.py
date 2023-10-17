af83f95af50082ad1efaebb02bdd49ff5747703d
lucas matheus <luquinhoso222kmatheus@gmail.com>
2023-10-17 10:47:06 -0300

    maior e menor nmr

---
+++ maiorEmenor.py
@@ -0,0 +1,16 @@
+a = int(input('promeiro valor'))
+b = int(input('segundo valor'))
+c = int(input('segundo valor'))
+menor = a
+if b < a and B < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+#verificando quem e o maior
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+print('O menor valor digitado foi {}'.format(menor))
+print('o maior valor digitado foi {}'.format(maior))
