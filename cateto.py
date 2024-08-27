89cbf2b2063a48d12cb82f98b9323522a907d7c6
Lucas Matheus <146854180+LucasMatheusu@users.noreply.github.com>
2024-08-27 16:57:47 -0300

    Delete cateto.py

--- cateto.py
+++
@@ -1,5 +0,0 @@
-from math import hypot
-cat = float(input("Comprimento do  cateto"))
-ca = float(input("comprimento do cateto  adja"))
-h1 = hypot(cat, ca)
-print("A hipotenusa vai ser {:.2f}".format(h1))
